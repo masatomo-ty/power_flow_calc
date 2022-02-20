e523cff3c7c6082dd38bdf69a05fb913da094688
Masatomo Toyama <masa.70.fg@gmail.com>
2022-02-20 14:28:24 +0900

    update format sh script

--- format.sh
+++ format.sh
@@ -1,20 +1,39 @@
-for day in {1..9} ; do
-    echo 120${day}
-	paste -d , 2010120${day}_11316/Load_1m2010120${day}_11316_TMN00*.csv > ./LOAD/LOAD_120${day}.csv
-	paste -d , 2010120${day}_11316/PVoutput_1m2010120${day}_11316_TMN00*.csv > ./PVOUT/PVOUT_120${day}.csv
+# cd ./45371
+# mkdir LOAD
+# mkdir PVOUT
+# find all and replace 45371 -> xxxxx if you want
+
+for mt in {1..9} ; do
+	for day in {1..9} ; do
+	    echo 0${mt}0${day}
+		paste -d , 20100${mt}0${day}_45371/Load_1m20100${mt}0${day}_45371_TMN00*.csv > ./LOAD/LOAD_0${mt}0${day}.csv
+		paste -d , 20100${mt}0${day}_45371/PVoutput_1m20100${mt}0${day}_45371_TMN00*.csv > ./PVOUT/PVOUT_0${mt}0${day}.csv
+	done
+
+	for day in {10..31} ; do
+	    echo 0${mt}${day}
+		paste -d , 20100${mt}${day}_45371/Load_1m20100${mt}${day}_45371_TMN00*.csv > ./LOAD/LOAD_0${mt}${day}.csv
+		paste -d , 20100${mt}${day}_45371/PVoutput_1m20100${mt}${day}_45371_TMN00*.csv > ./PVOUT/PVOUT_0${mt}${day}.csv
+	done
 done
 
-for day in {10..31} ; do
-    echo 12${day}
-	paste -d , 201012${day}_11316/Load_1m201012${day}_11316_TMN00*.csv > ./LOAD/LOAD_12${day}.csv
-	paste -d , 201012${day}_11316/PVoutput_1m201012${day}_11316_TMN00*.csv > ./PVOUT/PVOUT_12${day}.csv
+for mt in {10..12} ; do
+	for day in {1..9} ; do
+	    echo ${mt}0${day}
+		paste -d , 2010${mt}0${day}_45371/Load_1m2010${mt}0${day}_45371_TMN00*.csv > ./LOAD/LOAD_${mt}0${day}.csv
+		paste -d , 2010${mt}0${day}_45371/PVoutput_1m2010${mt}0${day}_45371_TMN00*.csv > ./PVOUT/PVOUT_${mt}0${day}.csv
+	done
+
+	for day in {10..31} ; do
+	    echo ${mt}${day}
+		paste -d , 2010${mt}${day}_45371/Load_1m2010${mt}${day}_45371_TMN00*.csv > ./LOAD/LOAD_${mt}${day}.csv
+		paste -d , 2010${mt}${day}_45371/PVoutput_1m2010${mt}${day}_45371_TMN00*.csv > ./PVOUT/PVOUT_${mt}${day}.csv
+	done
 done
 
 cat ./LOAD/LOAD_*.csv > load_2010.csv
 cat ./PVOUT/PVOUT_*.csv > pv_2010.csv
 
-cat ./LOAD/LOAD_04*.csv > load_201004.csv
-cat ./PVOUT/PVOUT_04*.csv > pv_201004.csv
-
-paste -d , 20101201_11316/Load_1m20101201_11316_TMN00*.csv > ./LOAD/LOAD_1201.csv
-paste -d , 20101201_11316/PVoutput_1m20101201_11316_TMN00*.csv > ./PVOUT/PVOUT_1201.csv
+# you can make it as month
+# cat ./LOAD/LOAD_04*.csv > load_201004.csv
+# cat ./PVOUT/PVOUT_04*.csv > pv_201004.csv
